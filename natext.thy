d778d85fd8159057a81c01d9f44e26020507ed3d
Yat  Wong <yat.wong18@imperial.ac.uk>
2020-07-20 20:43:10 +0800

    Added several theorems

--- natext.thy
+++ natext.thy
@@ -1,8 +1,39 @@
 theory natext
-imports ZF.Nat
+imports ZF.Arith
 begin
 
-definition nats :: "i ⇒ i" where
+find_theorems "_ #- _"
+find_theorems name: raw_diff
+
+definition nats :: "i \<Rightarrow> i" where
   "nats(n) == {m:nat. m < n}"
+
+theorem le_in_succ: "[| i \<le> n; n: nat; i:nat|] ==> i:succ(n)"
+apply(auto simp: ltD ltE)
+done
+
+theorem succ_add_1: "n:nat ==> succ(n) = n #+ 1"
+apply(auto)
+done
+
+theorem diff_le_0: "[| m \<le> n; n:nat; m:nat |] ==> m #- n = 0"
+apply(auto simp: diff_def)
+oops
+
+theorem add_lt_n: "[| x #+ n < y #+ n; n:nat; x:nat; y:nat |] ==> x < y"
+oops
+
+theorem diff_lt_0: "[| m < n; n:nat; m:nat |] ==> 0 < n #- m"
+oops
+
+theorem diff_add_comm: "[| m < n; n:nat; m:nat; k:nat|] ==> n #+ k #- m = k #+ (n #- m)"
+oops
+
+
+
+
+
+
+
 
 end
