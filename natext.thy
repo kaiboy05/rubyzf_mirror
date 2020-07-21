3e60fe4badaced3107f453732a8c478728847512
Yat  Wong <yat.wong18@imperial.ac.uk>
2020-07-21 16:24:07 +0800

    Added all the theorems in natext, going to prove them

--- natext.thy
+++ natext.thy
@@ -2,9 +2,6 @@ theory natext
 imports ZF.Arith
 begin
 
-find_theorems "_ #- _"
-find_theorems name: raw_diff
-
 definition nats :: "i \<Rightarrow> i" where
   "nats(n) == {m:nat. m < n}"
 
@@ -29,11 +26,51 @@ oops
 theorem diff_add_comm: "[| m < n; n:nat; m:nat; k:nat|] ==> n #+ k #- m = k #+ (n #- m)"
 oops
 
+theorem n_gt_0_succ: "[| 0 < n; n: nat; !!x. [| n = succ(x); x:nat |] ==> P |] ==> P"
+by(erule zero_lt_natE)
 
+theorem mult_le_self: "[| 0 < m; n:nat; m:nat |] ==> n \<le> n #* m"
+oops
 
+theorem gt_not0: "[| 0 < n; n:nat |] ==> n~= 0"
+oops
 
+theorem diff_succ: "[| m \<le> n; n:nat; m:nat |] ==> succ(n) #- m = succ(n #- m)"
+oops
 
+theorem diff_diff_inverse: "[| m < n; n:nat; m:nat |] ==> n #- (n #- m) = m"
+oops
 
+theorem add_0_eq_0: "[| n #+ m = 0; m:nat; n:nat |] ==> n = 0"
+oops
+
+theorem mult_cancel: "[| n #* m = n #* p; 0 < n; n:nat; m:nat; p:nat |] ==> m = p"
+oops
+
+theorem mod_dep_type: "[| 0 < n; m:nat; n:nat |] ==> m mod n :nats(n)"
+oops
+
+theorem mod_lt: "[| 0 < n; m:nat; n:nat |] ==> m mod n < n"
+oops
+
+theorem natsI: "[| m:nat; n:nat; m:n |] ==> m :nats(n)"
+oops
+
+theorem natsI2: "q:{ m:nat. m < n } ==> q: nats(n)"
+by(simp add: nats_def)
+
+theorem natsE: "[| m:nats(n); n:nat; [| m:nat; m < n |] ==> P |] ==> P"
+oops
+
+theorem ball_lt_nats: "[| ALL j:nats(succ(n)). p(j); ALL j:nats(n). p(j) ==> P; n:nat |] ==> P"
+oops
+
+theorem mod_mult_self: "[| t:nat; n:nat; 0 < n; j:nats(n) |] ==> (n #* t #+ j) mod n = j"
+oops
+
+theorem mod_diff: "[| t:nat; 0 < t; n:nat; 0 < n; j:nats(n) |] ==> 
+                    (n #* t #- j) mod n = (n #- j) mod n"
+oops
 
 
 end
